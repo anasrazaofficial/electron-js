f75a9cea15ef7a298fafd2afe2ad9f2cd63b9d21
sanasraza <sanasraza@techieonix.io>
2025-03-11 16:10:43 +0500

    Revert "Set up app"
    This reverts commit a4510f4ca130647ee7deb14d185b8d2dab02270d.

--- src/electron/main.js
+++
@@ -1,7 +0,0 @@
-import { app, BrowserWindow } from "electron";
-import { join } from "path";
-
-app.on("ready", () => {
-    const mainWindow = new BrowserWindow({});
-    mainWindow.loadFile(join(app.getAppPath() + "/dist-react/index.html"));
-});
